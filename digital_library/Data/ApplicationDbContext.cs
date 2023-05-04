12502ff55147e3ff3cd4ecdee61b536ea8c0bf50
99887 <99887@DESKTOP-HA0OHO6>
2023-05-05 00:00:58 +0330

    Add project files.

---
+++ digital_library/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace digital_library.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
